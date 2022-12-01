e6231d7ec482f35de67eb6239f4e648115fb00da
john thornton <jb087011@gmail.com>
2022-12-01 11:39:13 -0500

    Setup the business and create starter code
    Come up with business name, 10 menu items, Javadocs, and test the main method

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FoodCounterLab
  Intro to Computer Science I Lab - UNC Charlotte
+
+Key Concepts
+{
+    Array declaration and creation/instantiation
+    Array traversal for assigning values
+    Array traversal for retrieving values
+    Using different iteration/loops with arrays
+    Combining flow control (sequence, decision, iteration) in using arrays 
+}
+    
+
---
+++ ThorntonJohnBusinessDriver.java
@@ -0,0 +1,30 @@
+/**
+ * ITSC1212-103 - Professor von Briesen
+ * @author John Thornton
+ * @since December 1, 2022
+ * @version 1.0
+ * Setting up a basic business using arrays, objects, and methods
+ * 
+ */
+
+/*
+ * Business Name: John's Street Venezuela
+ * Menu Items:
+ *  1) Small drink
+ *  2) Medium drink
+ *  3) Large drink
+ *  4) Hallacas
+ *  5) Pabellon Taco (with option of bowl)
+ *  6) Empanadas (Chicken, Beef)
+ *  7) Arepas (Chicken, Beef)
+ *  8) Pastelas (Chicken, Beef)
+ *  9) Venezuelan Hot Dog (Papitas, Onions, Ketchup, Salsa Rosada)
+ *  10) Desserts (Cocosette, Guava & Cheese Empanada, Nutella Tequeños)
+ */
+public class ThorntonJohnBusinessDriver
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+}
